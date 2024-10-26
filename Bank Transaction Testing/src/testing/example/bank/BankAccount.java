ad9badf69be4e1ca54fa6d8b9b6dab94edd1f2eb
arvind088 <avi.lmc098@gmail.com>
2024-10-26 17:52:58 +0200

    first commit

---
+++ Bank Transaction Testing/src/testing/example/bank/BankAccount.java
@@ -0,0 +1,37 @@
+package testing.example.bank;
+
+public class BankAccount {
+
+    private int id;
+    private double balance = 0;
+    private static int lastId = 0;
+
+    public BankAccount() {
+        this.id = ++lastId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount < 0) {
+            throw new IllegalArgumentException("Negative amount: " + amount);
+        }
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        if (amount < 0) {
+            throw new IllegalArgumentException("Negative amount: " + amount);
+        }
+        if (balance < amount) {
+            throw new IllegalArgumentException("Cannot withdraw " + amount + " from " + balance);
+        }
+        balance -= amount;
+    }
+}
